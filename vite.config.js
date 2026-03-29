63acbb97c402de4ade6bb2339a6ca5ce60cf689a
savitskiy znak <152868273+ZNAKLATINSKIY@users.noreply.github.com>
2026-03-30 02:58:37 +0500

    vitesdela

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
  
 export default defineConfig({
+  base: '/ZooDashaTopia/', 
   plugins: [vue()],
   server: { port: 5173 }
 })
